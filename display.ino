53c86e40116cb7075d58727088205ff8dd73e0ab
Harley Wakeman <123672121+hwakeman@users.noreply.github.com>
2024-03-31 20:41:08 -0400

    Added NBA Functionality

--- display.ino
+++ display.ino
@@ -1,40 +1,47 @@
 #include <LiquidCrystal_I2C.h>
+#include <WiFiUdp.h>
+#include <NTPClient.h>
 #include <ESP8266WiFi.h>
 #include <WiFiClientSecure.h>
 #include <ArduinoJson.h>
 
-// THESE ARE THE VALUES THAT MUST BE CHANGED
+// THESE ARE THE VALUES THAT MUST BE CHANGED /////////////////////////////////////
 
-// WiFi credentials
-char ssid[] = "";
-char password[] = "";
+  // WiFi credentials
+  char ssid[] = "";
+  char password[] = "";
 
-// Index of the sport (Check README for your sport's number)
-int apiIndex = 0;
+  // Index of the sport (Check README for your sport's number)
+  int apiIndex = 0;
 
-// Index of the team (Check README for your team's number)
-int teamIndex = 0;
+  // Index of the team (Check README for your team's number)
+  int teamIndex = 0;
 
-// How often you want the scores to update in minutes
-const long minutes = 5;
+  // How often you want the scores to update in minutes
+  const long minutes = 1;
 
-////////////////////////////////////////////
+//////////////////////////////////////////////////////////////////////////////////
 
 WiFiClientSecure client;
 
+
+WiFiUDP ntpUDP;
+NTPClient timeClient(ntpUDP, "pool.ntp.org", -18000, 60000);
+
 String nhlTeams[] = {"ANA", "ARI", "BOS", "BUF", "CGY", "CAR", "CHI", "COL", "CBJ", "DAL", "DET", "EDM", "FLA", "LAK", "MIN", "MTL", "NSH", "NJD", "NYI", "NYR", "OTT", "PHI", "PIT", "SJS", "SEA", "STL", "TBL", "TOR", "VAN", "VGK", "WSH", "WPG"};
-int teamIds[4][32] = {
+int teamIds[5][32] = {
   {57, 58, 402, 397, 328, 61, 354, 62, 341, 338, 64, 65, 66, 67, 68, 340, 73, 346, 563, 76, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0},
-  {108, 109, 110, 111, 112, 113, 114, 115, 116, 117, 118, 119, 120, 121, 133, 134, 135, 136, 137, 138, 139, 140, 141, 142, 143, 144, 145, 146, 147, 158, 0, 0},
+  {109, 144, 110, 111, 112, 145, 113, 114, 115, 116, 117, 118, 108, 119, 146, 158, 142, 121, 147, 133, 143, 134, 135, 137, 136, 138, 139, 140, 141, 120},
   {0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0},
-  {1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20, 21, 22, 23, 24, 25, 26, 27, 28, 29, 30, 31, 32}
+  {1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20, 21, 22, 23, 24, 25, 26, 27, 28, 29, 30, 31, 32},
+  {1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20, 21, 22, 23, 24, 25, 26, 27, 28, 29, 30},
 };
 
-const char* baseUrls[] = {"api.football-data.org", "statsapi.mlb.com", "api-web.nhle.com", "tank01-nfl-live-in-game-real-time-statistics-nfl.p.rapidapi.com"};
-const char* apiKeys[] = {"9b058c7bb14b4b48bde465ad8783823b", "none", "none", "8397916b8dmshc4fe775ebddd378p1c43ffjsn577a3cae20a4"};
-const char* firstHalfQueries[] = {"/v4/teams/", "/api/v1/schedule/games/?sportId=1&teamId=", "/v1/scoreboard/", "/getNFLTeamSchedule?teamID="};
-const char* secondHalfQueries[] = {"/matches?status=FINISHED,IN_PLAY&limit=1", "", "/now", ""};
-const char* apiNames[] = {"Premier League", "MLB", "NHL", "NFL"};
+const char* baseUrls[] = {"api.football-data.org", "statsapi.mlb.com", "api-web.nhle.com", "tank01-nfl-live-in-game-real-time-statistics-nfl.p.rapidapi.com", "api.balldontlie.io"};
+const char* apiKeys[] = {"9b058c7bb14b4b48bde465ad8783823b", "none", "none", "8397916b8dmshc4fe775ebddd378p1c43ffjsn577a3cae20a4", "6ac6a384-ff48-4c6a-9a60-9c4103f6638c"};
+const char* firstHalfQueries[] = {"/v4/teams/", "/api/v1/schedule/games/?sportId=1&teamId=", "/v1/scoreboard/", "/getNFLTeamSchedule?teamID=", "/v1/games?team_ids[]="};
+const char* secondHalfQueries[] = {"/matches?status=FINISHED,IN_PLAY&limit=1", "", "/now", "", ""};
+const char* apiNames[] = {"Premier League", "MLB", "NHL", "NFL", "NBA"};
 
 const int i2c_address = 0x27;
 const int lcd_columns = 16;
@@ -59,6 +66,8 @@ void setup() {
     delay(500);
   }
 
+  timeClient.begin();
+
   makeHTTPRequest();
 }
 
@@ -73,42 +82,38 @@ void loop() {
   }
 }
 
-void printScores(String homeName, String awayName, int homeScore, int awayScore) {
-  lcd.setCursor(0,0);
-  lcd.print(homeName);
-  lcd.print(": ");
-  lcd.print(homeScore);
-  lcd.setCursor(0, 1);
-  lcd.print(awayName);
-  lcd.print(": ");
-  lcd.print(awayScore);
-}
-
-int getEffectiveLength(int array[], int maxSize) {
-  for (int i = 0; i < maxSize; i++) {
-    if (array[i] == 0) {
-      return i; 
-    }
-  }
-  return maxSize;
-}
-
 void makeHTTPRequest() {
   if (!client.connect(baseUrls[apiIndex], 443)) {
-    Serial.println(F("Connection failed"));
+    lcd.println(F("Connection failed"));
     return;
   }
 
-  client.print(F("GET "));
-  client.print(firstHalfQueries[apiIndex]);
+  String request = F("GET ");
+  request += firstHalfQueries[apiIndex];
   if(apiIndex == 2) {
-    client.print(nhlTeams[teamIndex % 30]);
+    request += nhlTeams[teamIndex % 32];
   }
   else {
-    client.print(teamIds[apiIndex][teamIndex % getEffectiveLength(teamIds[apiIndex], 32)]);
+    request += teamIds[apiIndex][teamIndex % getEffectiveLength(teamIds[apiIndex], 32)];
   }
-  client.print(secondHalfQueries[apiIndex]);
-  client.println(F(" HTTP/1.1"));
+  request += secondHalfQueries[apiIndex];
+  
+  if(apiIndex == 4) {
+    request += "&dates[]=";
+
+    timeClient.update();
+    unsigned long epochTime = timeClient.getEpochTime();
+    struct tm *timeInfo;
+    time_t rawTime = (time_t)epochTime;
+    timeInfo = localtime(&rawTime);
+    char formattedDate[11];
+    snprintf(formattedDate, sizeof(formattedDate), "%04d-%02d-%02d", timeInfo->tm_year + 1900, timeInfo->tm_mon + 1, timeInfo->tm_mday);
+    request += formattedDate;
+  }
+
+  request += F(" HTTP/1.1");
+
+  client.println(request);
 
   client.print(F("Host: "));
   client.println(baseUrls[apiIndex]);
@@ -166,6 +171,10 @@ void makeHTTPRequest() {
 
     if(apiIndex == 3) {
       makeNflRequest(root);
+    }
+
+    if(apiIndex == 4) {
+      makeNbaRequest(root);
     }
   }
 }
@@ -266,4 +275,48 @@ void makeNflRequest(JsonObject root) {
       return;
     }
   }
+}
+
+void makeNbaRequest(JsonObject root) {
+  JsonArray data = root["data"].as<JsonArray>();
+  JsonObject todaysGame = data[0].as<JsonObject>();
+
+  int homeScore = todaysGame["home_team_score"].as<int>();
+  int awayScore = todaysGame["visitor_team_score"].as<int>();
+
+  JsonObject homeTeam = todaysGame["home_team"].as<JsonObject>();
+  JsonObject awayTeam = todaysGame["visitor_team"].as<JsonObject>();
+
+  String homeName = homeTeam["name"].as<String>();
+  String awayName = awayTeam["name"].as<String>();
+
+  if(homeName == "null") {
+    lcd.setCursor(0, 0);
+    lcd.print("No games");
+    lcd.setCursor(0, 1);
+    lcd.print("today");
+  } else {
+    printScores(homeName, awayName, homeScore, awayScore);
+  }
+
+}
+
+void printScores(String homeName, String awayName, int homeScore, int awayScore) {
+  lcd.setCursor(0,0);
+  lcd.print(homeName);
+  lcd.print(": ");
+  lcd.print(homeScore);
+  lcd.setCursor(0, 1);
+  lcd.print(awayName);
+  lcd.print(": ");
+  lcd.print(awayScore);
+}
+
+int getEffectiveLength(int array[], int maxSize) {
+  for (int i = 0; i < maxSize; i++) {
+    if (array[i] == 0) {
+      return i; 
+    }
+  }
+  return maxSize;
 }
